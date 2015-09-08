59e1bc26e551b8edfff6f320c9cacbf7d792d01f
agentreno <kturrell@hotmail.com>
2015-09-08 22:14:26 +0100

    Number examples

---
+++ chapter2/number.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+print("11 divided by 2 and truncated: " + str(11 // 2))
+print("-11 // 2 = " + str(-11 // 2))
+print("11.0 // 2 = " + str(11.0 // 2))
+print("11 to the power of 2: 11 ** 2 = " + str(11 ** 2))
+print("11 mod 2: 11 % 2 = " + str(11 % 2))
+import fractions
+x = fractions.Fraction(1,3)
+print(str(x) + " * 2 = " + str(x*2))
+import math
+print("math.pi is " + str(math.pi))
+print("sin(pi/2) is " + str(math.sin(math.pi / 2)))
