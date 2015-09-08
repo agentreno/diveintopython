99031e7b8c578fb7884799cb071d89cd27e5ff0c
agentreno <kturrell@hotmail.com>
2015-09-08 19:48:35 +0100

    Docstring example

---
+++ chapter1/docstring.py
@@ -0,0 +1,13 @@
+def myfunc():
+   """Demonstrate a one-line docstring and return None."""
+
+def myfunc2(a=1, b=2):
+   """Demonstrate a multi-line docstring.
+
+   Keyword arguments:
+   a - meaningless argument (default 1)
+   b - meaningless argument (default 2)
+
+   Returns:
+   None
+   """
