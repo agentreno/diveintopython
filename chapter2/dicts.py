555769ac435ad04288169248b0f66644dc521dc6
Karl <kturrell@hotmail.com>
2015-09-17 21:27:21 +0100

    Dictionary assignment/copying example

--- chapter2/dicts.py
+++ chapter2/dicts.py
@@ -3,3 +3,10 @@ a_dict = {'a':1, 'b':2}
 a_dict['c'] = 3
 len(a_dict)
 'a' in a_dict
+a_refdict = a_dict
+a_refdict['d'] = 4
+print("Assignment doesn't copy dictionaries: " + str(a_dict))
+import copy
+a_copydict = copy.copy(a_refdict)
+del a_refdict['d']
+print("But the copy module can copy dictionaries: " + str(a_copydict))
